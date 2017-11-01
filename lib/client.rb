fb03748b9d29e1ec45fad4083af0a6de8fe54251
Calvin McElroy <fourwallsstudio@gmail.com>
2017-10-31 23:20:12 -0400

    fix response

--- lib/client.rb
+++ lib/client.rb
@@ -4,10 +4,12 @@ class Client
   URL = 'http://localhost'
 
   def self.connect(port, peers, blockchain)
-    Faraday.post("#{URL}:#{port}/connect", peers: peers, blockchain: blockchain)
+    Faraday.post("#{URL}:#{port}/connect", peers: peers, blockchain: blockchain).body
   end
 
   def self.send(port, to, amount)
-    Faraday.post("#{URL}:#{port}/send", to: to, amount: amount)
+    Faraday.post("#{URL}:#{port}/send", to: to, amount: amount).body
   end
 end
+
+puts Client.connect(1234, 'peerrrss', 'bc')
