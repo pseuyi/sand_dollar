48e2e5073fd18d735b5cdbc83ce01afba5decfc8
Calvin McElroy <fourwallsstudio@gmail.com>
2017-10-31 22:51:13 -0400

    create client class

--- lib/client.rb
+++ lib/client.rb
@@ -1,1 +1,20 @@
 require 'faraday'
+
+class Client
+  URL = 'http://localhost'
+
+  def self.gossip(port, peers, blockchain)
+    begin
+      Faraday.post("#{URL}:#{port}/gossip", peers: peers, blockchain: blockchain)
+    rescue Faraday::ConnectionFailed => e
+      raise
+    end
+  end
+
+  def self.send_money(port, to, amount)
+    Faraday.post("#{URL}:#{port}/send_money", to: to, amount: amount)
+  end
+end
+
+
+p Client.send_money(9999, 8888, 300)
